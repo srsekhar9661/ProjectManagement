1e4e1d9eb8cafb26ba050cd32254f8b20bfcd48a
srsekhar9661 <srsekhar9661@gmail.com>
2026-03-21 21:18:29 +0530

    Removed __pycache__ and applied .gitignore

--- ProjectManagement/api/models.py
+++ ProjectManagement/api/models.py
@@ -16,3 +16,37 @@ class Project(models.Model):
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     organization_name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.user.username
+
+ROLES = [
+    ('admin', 'admin'),
+    ('ProjectManager', 'ProjectManager'),
+    ('developer', 'developer'),
+]
+
+class ProjectMembership(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    project = models.ForeignKey('Project', on_delete=models.CASCADE)
+    role = models.CharField(max_length=50, choices=ROLES, default='member')
+
+    def __str__(self):
+        return f'{self.user.username} - {self.project.name}'
+    
+
+class Task(models.Model):
+    STATUS_CHOICES = [
+        ('todo', 'To Do'),
+        ('progress', 'In Progress'),
+        ('done', 'Done')
+    ]
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    project = models.ForeignKey('Project', on_delete=models.CASCADE, related_name='tasks')
+    status  = models.CharField(max_length=20, choices=STATUS_CHOICES, default='todo')
+    created_at =models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+    
