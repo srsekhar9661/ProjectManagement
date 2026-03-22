e0009d41378094d21c52548541d184af19bb7dc9
srsekhar9661 <srsekhar9661@gmail.com>
2026-03-22 08:04:30 +0530

    Added the refresh token related code if the code breaks and not receiving the data as token got expired

--- project-m/src/api.ts
+++ project-m/src/api.ts
@@ -1,3 +1,4 @@
+import { Config } from './../node_modules/@eslint/config-helpers/dist/esm/index.d';
 import axios from "axios";
 
 const API = axios.create({
@@ -12,5 +13,45 @@ API.interceptors.request.use((config)=>{
     }
     return config
 })
+
+// Response interceptor ( key part )
+API.interceptors.response.use(
+    (response) => response,
+    async (error) => {
+        const originalRequest = error.Config
+        if (
+            error.response?.status === 401 && 
+            !originalRequest._retry
+        ) {
+            originalRequest._retry = true
+
+            try {
+                const refresh = localStorage.getItem('refresh')
+
+                const res = await axios.post(
+                    'http://127.0.0.1:8000/api/token/refresh/',
+                    { refresh }
+                )
+
+                // save the new access token
+                localStorage.setItem('access', res.data.access)
+
+                // retry original request
+                originalRequest.headers.Authorization = `Bearer ${res.data.access}`
+
+                return API(originalRequest)
+            } catch (err) {
+                // refresh failed - logout
+                localStorage.removeItem('access')
+                localStorage.removeItem('refresh')
+
+                window.location.href = '/login'
+            }
+        }
+
+        return Promise.reject(error)
+    }
+)
+
 
 export default API
